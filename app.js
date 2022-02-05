6dd2383301c0f31d00eeff814a7c64861df79f69
Jasminethecreator <jasminebienaime@outlook.com>
2022-02-04 21:31:41 -0500

    declare variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('this is a test:hi Jasmine')
 
-
+// Variables
+let cards= message,matchesRemaining, turn, seconds,tickInterval, waitingForTimeout,card1Indx, card1Val,card2Idx,card2Val
 
